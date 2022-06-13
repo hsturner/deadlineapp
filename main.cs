1a929a61e740b9315e283b152ac3f37a0d601fba
harlan <hsavinturner@gmail.com>
2022-06-13 16:02:58 -0400

    added additional tests for user class

--- main.cs
+++ main.cs
@@ -1,6 +1,8 @@
 using System;
 using Task_object;
+using Task_user;
 
+namespace TestClass{
 class TestClass
 {
     static void Main(string[] args)
@@ -10,5 +12,17 @@ class TestClass
         Console.WriteLine(test1.Assigned());
         Console.WriteLine(test1.Due());
         Console.WriteLine(test1.Remaining());
+
+
+
+
+        User user1 = new User ("harlan", "test passwword");
+        user1.createTask("Test task via createTask function", "6/21/22");
+        user1.createTask("test task 2 added to users dictionary", "6/31/2025", "5/13/2022");
+
+        user1.allTasks();
+        
+
     }
+}
 }
